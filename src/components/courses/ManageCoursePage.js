bfc8a7d60e5de07095c8eca90fa5b0967e034574
mcmcgovern <michaelmcgovern120@gmail.com>
2023-02-05 22:10:31 -0500

    api call error handling

--- src/components/courses/ManageCoursePage.js
+++ src/components/courses/ManageCoursePage.js
@@ -6,6 +6,7 @@ import PropTypes from "prop-types";
 import CourseForm from "../courses/CourseForm";
 import { newCourse } from "../../../tools/mockData";
 import Spinner from "../common/Spinner";
+import { toast } from "react-toastify";
 
 function ManageCoursePage({
     courses,
@@ -18,6 +19,7 @@ function ManageCoursePage({
 }) {
     const [course, setCourse] = useState({ ...props.course });
     const [errors, setErrors] = useState({});
+    const [saving, setSaving] = useState(false);
     useEffect(() => {
         if (courses.length === 0) {
             loadCourses().catch((error) => {
@@ -43,9 +45,16 @@ function ManageCoursePage({
 
     function handleSave(event) {
         event.preventDefault;
-        saveCourse(course).then(() => {
-            history.push("/courses");
-        });
+        setSaving(true);
+        saveCourse(course)
+            .then(() => {
+                toast.success("Course saved");
+                history.push("/courses");
+            })
+            .catch((error) => {
+                setSaving(false);
+                setErrors({ onSave: error.message });
+            });
     }
 
     return authors.length === 0 || courses.length === 0 ? (
@@ -57,6 +66,7 @@ function ManageCoursePage({
             authors={authors}
             onChange={handleChange}
             onSave={handleSave}
+            saving={saving}
         />
     );
 }
